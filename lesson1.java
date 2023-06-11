bde6a5a6b678c92849d5fd48bd9181f8dc7f5afc
shih674 <shih820503@gmail.com>
2023-06-12 01:02:14 +0800

    lesson1 finish

--- lesson1.java
+++ lesson1.java
@@ -11,23 +11,33 @@ public class lesson1{
     public static void main(String[] args){
         // 在終端機印出各種資料型態的資料
         // 整數，預設是int型態
-        System.out.println(3);
-        // 長整數，數字的後面特別加上Ｌ
-        System.out.println(2000000000L);
-        // 浮點數，預設為double型態
-        System.out.println(3.14159268);
-        // 單精度浮點數，數字的後面特別加上Ｆ：到小數點後第七位
-        System.out.println((3.14159268F));
-        // 布林值
-        System.out.println(false);
-        // 字元：單引號。
-        System.out.println('a');
-        // 字串：雙引號。可包含０到多個字元
-        System.out.println("哈囉你好");
-        
-        //變數
         int x = 3;
-        System.out.println(x); 
+        System.out.println(x);
+        // 長整數，數字的後面特別加上Ｌ
+        long y = 20000000000L;
+        System.out.println(y);
+
+        // 浮點數，預設為double型態
+        double m = 3.14159268;
+        System.out.println(m);
+        // 單精度浮點數，數字的後面特別加上Ｆ：到小數點後第七位
+        float n = 3.14159268F;
+        System.out.println((n));
+
+        // 布林值
+        boolean b;
+        b = true;
+        System.out.println(b);
+
+        // 字元：單引號。
+        char s = 'a';
+        System.out.println(s);
+        // 字串：雙引號。可包含０到多個字元
+        String t = "哈囉你好";
+        System.out.println(t);
+        t= "你也好";
+        System.out.println(t);
+        
     }
 }
 
